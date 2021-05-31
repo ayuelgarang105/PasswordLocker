07a2d1a8f48c94b8248cebfc73544f37ebf0b7d2
Gabriel Ayuel <ayuelgarang105@gmail.com>
2021-05-31 10:29:30 +0300

    add user email

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import pyperclip
 
 class User:
     userList = []
-    def __init__(self, user_name, password):
+    def __init__(self, user_name, password, email):
         self.user_name = user_name
         self.password = password
         self.user_name = email
